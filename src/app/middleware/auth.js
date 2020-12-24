b590a1fa97a10881a26b3dfdee3a07324fb67347
Jair Junior <contato@jairjunior.eng.br>
2020-12-24 20:10:44 -0300

    Error messages have been changed.

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -5,18 +5,18 @@ module.exports = (req, res, next) => {
      const authHeader = req.headers.authorization;
 
      if( !authHeader )
-          return res.status(401).send({ error: 'The access token must be provided.' });
+          return res.status(401).send({ error: 'No token provided (Unauthorized).' });
 
      const parts = authHeader.split(' ');
      if(!parts.length === 2)
-          return res.status(401).send({ error: 'Token error.' });
+          return res.status(401).send({ error: 'Token error (Unauthorized).' });
 
      const [ scheme, token ] = parts;
      if(! /^bearer$/i.test(scheme))
-          return res.status(401).send({ error: 'Token malformatted.' });
+          return res.status(401).send({ error: 'Token malformatted (Unauthorized).' });
 
      jwt.verify(token, process.env.APP_AUTH_HASH, (err, decoded) => {
-          if(err) return res.status(401).send({ error: 'Invalid token.' });
+          if(err) return res.status(401).send({ error: 'Invalid token (Unauthorized).' });
 
           // After validate the token, the user id will be available as a result of the decryption
           // So it will be saved in the "req" variable to be used by the rest of the code.
