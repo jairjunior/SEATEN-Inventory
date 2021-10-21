7e110774bae5fe52f35088ea8bf4e41ea4d1576f
Jair Dias de Oliveira Junior <jair.junior@cjf.jus.br>
2021-10-21 11:11:23 -0300

    Register Form layout modifield. Addded fieldsets to organize content. In JS file there's a function to show each fieldset. By default, the form shows only the first fieldset with the Item Specs to choose. After choose category and model, the other fieldsets will show up beneath.

--- public/js/register.js
+++ public/js/register.js
@@ -115,7 +115,6 @@ $('#selectItemModel').change( eventHandler => {
     showModelSpecs(modelId);
 });
 
-
 //----------------------------------------------------------------------------------------
 // 
 //----------------------------------------------------------------------------------------
@@ -127,24 +126,15 @@ function showModelSpecs(modelId){
 
     const itemModelsList = JSON.parse( localStorage.getItem('itemModels') );
     let selectedModel = itemModelsList.filter( model => { return model._id === modelId });
-    const inventoryPrefix = 'CJF '
     console.log(selectedModel[0]);
 
-    $('#modelSpecs').remove();
+    if( $('#modelSpecs').length > 0 ) $('#modelSpecs').remove();
+
     $('#selectItemModel').after(`
     <div id='modelSpecs'>
-        <h5>Model specifications:</h5>
+        <h5>Model Specifications:</h5>
         <ul class='pl-4' id='ListOfModelSpecs'></ul>
-        <div class="form-group">
-            <label for="inputInventoryNumber">Inventory Number:</label>
-            <input class="form-control" id="inputInventoryNumber" type="text" value="${inventoryPrefix}">
-        </div>
-        <button type="button" id="btnRegisterNewItem" class="btn btn-success">Register New Stock Item</button>
-    </div>`);    
-
-    inventoryNumberFieldCursorControl();
-    inventoryNumberFieldInputRules();
-    inventoryNumberFieldDotControl();
+    </div>`);
 
     const specs = selectedModel[0].specs;
      for(let spec in specs){
@@ -153,6 +143,35 @@ function showModelSpecs(modelId){
      }
 }
 
+
+//----------------------------------------------------------------------------------------
+// 
+//----------------------------------------------------------------------------------------
+function showInventoryNumberFieldset(){
+    const inventoryPrefix = 'CJF '
+    const PREFIX_LENGTH = inventoryPrefix.length;
+    const INPUT_MAX_LENGTH = 11; // pattern: CJF 123.456
+
+    $('#fieldsetItemSpecs').after(`
+    <fieldset class="form-group" id="fieldsetInventoryNumber">
+        <legend>Inventory Number</legend>
+            <div class="form-row">
+                <div class="form-group col-md-12">
+                    <label for="inputInventoryNumber">Inventory Number:</label>
+                    <input type="text" class="form-control" id="inputInventoryNumber" name="inventoryNumber" aria-describedby="inputInventoryNumberFeedback" value="${inventoryPrefix}" required>
+                    <div id="inputInventoryNumberFeedback"></div>
+                </div>
+            </div>
+    </fieldset>`);
+
+    // The following functions add event listenners to the input field to format the text
+    inventoryNumberFieldCaretControl(PREFIX_LENGTH);
+    inventoryNumberFieldKeysControl(PREFIX_LENGTH, INPUT_MAX_LENGTH);
+    inventoryNumberFieldDotControl(PREFIX_LENGTH, INPUT_MAX_LENGTH);
+
+
+    //<button type="button" id="btnRegisterNewItem" class="btn btn-success">Register New Stock Item</button>
+}
 
 
 //----------------------------------------------------------------------------------------
@@ -268,3 +287,145 @@ function repositionCaret(eventHandler, currentCaretPosition){
     }
 }
 
+//----------------------------------------------------------------------------------------
+// 
+//----------------------------------------------------------------------------------------
+function showRequisitionInfoFieldset(){
+    $('#fieldsetInventoryNumber').after(`
+    <fieldset class="form-group" id="fieldsetRequisitionInfo">
+        <legend>Requisition Info</legend>
+        <div class="form-row">
+            <div class="form-group col-md-3">
+                <label for="selectReqType">Type</label>
+                <select id="selectReqType" class="form-control" name="reqType" required>
+                    <option selected>RITM</option>
+                    <option>INC</option>
+                </select>
+            </div>
+            <div class="form-group col-md-9">
+                <label for="inputReqNumber">Number</label>
+                <input type="tel" class="form-control" id="inputReqNumber" name="reqNumber" aria-describedby="inputReqNumberFeedback" placeholder="00017123" required>
+                <div id="inputReqNumberFeedback"></div>
+            </div>
+        </div>
+    </fieldset>`);
+}
+
+//----------------------------------------------------------------------------------------
+// 
+//----------------------------------------------------------------------------------------
+function showFromUserFieldset(){
+    $('#fieldsetRequisitionInfo').after(`
+    <fieldset class="form-group" id="fieldsetFromUser">
+        <legend>From</legend>
+        <div class="form-row">
+            <div class="form-group col-md-9">
+                <label for="fromUserName">Full User Name</label>
+                <input type="text" class="form-control" id="fromUserName" aria-describedby="fromUserNameFeedback" name="fromUserName" value="José Milton Severino Botelho" required>
+                <div id="fromUserNameFeedback"></div>
+            </div>
+            <div class="form-group col-md-3">
+                <label for="fromRegistrationNumber">Registration Number</label>
+                <input type="tel" class="form-control" id="fromRegistrationNumber" aria-describedby="fromRegistrationNumberFeedback" name="fromRegistrationNumber" value="129" required>
+                <div id="fromRegistrationNumberFeedback"></div>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-4">
+                <label for="fromDivision">Division</label>
+                <input type="text" class="form-control" id="fromDivision" aria-describedby="fromDivisionFeedback" name="fromDivision" value="SAD" required>
+                <div id="fromDivisionFeedback"></div>
+            </div>
+            <div class="form-group col-md-4">
+                <label for="fromBranch">Branch</label>
+                <input type="text" class="form-control" id="fromBranch" aria-describedby="fromBranchFeedback" name="fromBranch" value="SUCOP" required>
+                <div id="fromBranchFeedback"></div>
+            </div>
+            <div class="form-group col-md-4">
+                <label for="fromDepartment">Department</label>
+                <input type="text" class="form-control" id="fromDepartment" aria-describedby="fromDepartmentFeedback" name="fromDepartment" value="SEMAPA" required>
+                <div id="fromDepartmentFeedback"></div>
+            </div>
+        </div>
+    </fieldset>`);
+}
+
+//----------------------------------------------------------------------------------------
+// 
+//----------------------------------------------------------------------------------------
+function showToUserFieldset(){
+    $('#fieldsetFromUser').after(`
+    <fieldset class="form-group" id="fieldsetToUser">
+        <legend>To</legend>
+        <div class="form-row">
+            <div class="form-group col-md-9">
+                <label for="ToUserName">Full User Name</label>
+                <input type="text" class="form-control" id="ToUserName" aria-describedby="ToUserNameFeedback" name="ToUserName" value="Valéria Prado Arcirio de Oliveira Braga" required>
+                <div id="ToUserNameFeedback"></div>
+            </div>
+            <div class="form-group col-md-3">
+                <label for="toRegistrationNumber">Registration Number</label>
+                <input type="tel" class="form-control" id="toRegistrationNumber" aria-describedby="toRegistrationNumberFeedback" name="toRegistrationNumber" value="457" required>
+                <div id="toRegistrationNumberFeedback"></div>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-4">
+                <label for="toDivision">Division</label>
+                <input type="text" class="form-control" id="toDivision" aria-describedby="toDivisionFeedback" name="toDivision" value="STI" required>
+                <div id="toDivisionFeedback"></div>
+            </div>
+            <div class="form-group col-md-4">
+                <label for="toBranch">Branch</label>
+                <input type="text" class="form-control" id="toBranch" aria-describedby="toBranchFeedback" name="toBranch" value="SUTEC" required>
+                <div id="toBranchFeedback"></div>
+            </div>
+            <div class="form-group col-md-4">
+                <label for="toDepartment">Department</label>
+                <input type="text" class="form-control" id="toDepartment" aria-describedby="toDepartmentFeedback" name="toDepartment" value="SEATEN" required>
+                <div id="toDepartmentFeedback"></div>
+            </div>
+        </div>
+    </fieldset>`);
+}
+
+//----------------------------------------------------------------------------------------
+// 
+//----------------------------------------------------------------------------------------
+function showReasonFieldset(){
+    $('#fieldsetToUser').after(`
+    <fieldset class="form-group" id="fieldsetReason">
+        <legend>Reason</legend>
+        <div class="form-row">
+            <div class="form-group col-md-12">
+                <label for='registrationReason'>Select:</label>
+                <select class='form-control' name='reason' id='registrationReason' aria-describedby="registrationReasonFeedback" required>
+                    <option value='New registration' selected>New registration</option>
+                    <option value='Donation'>Donation</option>
+                    <option value='Keep in stock'>Keep in stock</option>
+                    <option value='New installation'>New installation</option>
+                    <option value='Repair / Warranty'>Repair / Warranty</option>
+                    <option value='Replacement'>Replacement</option>
+                    <option value='Without use'>Without use</option>
+                </select>
+                <div id="registrationReasonFeedback"></div>
+            </div>
+        </div>
+    </fieldset>`);
+}
+
+//----------------------------------------------------------------------------------------
+// 
+//----------------------------------------------------------------------------------------
+function showRemarksFieldset(){
+    $('#fieldsetReason').after(`
+    <fieldset class="form-group" id="fieldsetRemarks">
+        <legend>Remarks</legend>
+        <div class="form-row">
+            <div class="form-group col-md-12">
+                <textarea class="form-control" id="registrationRemarks" name="remarks" rows='4' placeholder='Make any observation (optional)...' aria-describedby="remarksHelp"></textarea>
+            </div>
+        </div>
+    </fieldset>`);
+
+}
