5184f1ab4bb10c2cc642abf7fb526917c5286dc2
Jair Junior <contato@jairjunior.eng.br>
2021-12-31 00:15:28 -0300

    A tag link que adicionava o Bootstrap ao componente foi retirada e no lugar foi adicionado CSS correspondente para manter o padrão da biblioteca.

--- public/js/Alert.js
+++ public/js/Alert.js
@@ -3,21 +3,64 @@
 const template = document.createElement('template');
 template.innerHTML = `
      <style>
+          .alert {
+               margin-top: 3rem;
+               padding: 0.75rem 1.25rem;
+               border: 1px solid transparent;
+               border-radius: 0.25rem;
+          }
+          .alert-success {
+               color: #155724;
+               background-color: #d4edda;
+               border-color: #c3e6cb;
+          }
+          .alert-danger {
+               color: #721c24;
+               background-color: #f8d7da;
+               border-color: #f5c6cb;
+          }
+          .flex {
+               display: flex;
+               flex-wrap: nowrap;
+               justify-content: space-between;
+               align-items: center;
+          }
+          p {
+               display: inline-block;
+               margin: 0;
+          }
+          .close {
+               border: 0;
+               padding: 0;
+               font-size: 1.5rem;
+               font-weight: 700;
+               color: #000;
+               text-shadow: 0 1px 0 #fff;
+               opacity: 0.5;
+               line-height: 1;
+               cursor: pointer;
+               background-color: transparent;
+               font-family: inherit;
+          }
           .fade-out {
-               animation: fadeOut 1s ease-out forwards, shrink 1.5s ease-in forwards;
+               animation: fadeOut 0.5s ease-out forwards;
           }
           @keyframes fadeOut {
                from { opacity: 1; }
                to { opacity: 0; }
           }
-          @keyframes shrink {
-               100% { height: 0px; }
+          @media screen and (max-width: 991px){
+               .close {
+                    padding-left: 1rem;
+               }
           }
      </style>
-     <link rel="stylesheet" href="../css/bootstrap.min.css">
-     <div class="alert alert-dismissible fade show mt-5" role="alert">
-          <p class="m-0"><slot name="alertText" /></p>
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+     <div class="alert flex" role="alert">
+          <p>
+               <slot name="alertSymbol"></slot>
+               <slot name="alertText"></slot>
+          </p>
+          <button type="button" class="close" aria-label="Close">
                <span aria-hidden="true">&times;</span>
           </button>
      </div>
@@ -37,7 +80,7 @@ export default class Alert extends HTMLElement {
      connectedCallback(){
           this.shadowRoot.querySelector('button.close').addEventListener('click', () => {
                this.shadowRoot.querySelector('div.alert').classList.add('fade-out');
-               setTimeout( () => this.remove(), 1500);
+               setTimeout( () => this.remove(), 500);
           });
      }
 
